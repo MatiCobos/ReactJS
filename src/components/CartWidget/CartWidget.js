1de3ad0ada737fef3c63e1b6b59c003ad825bae4
matias <casla_matias@hotmail.com>
2022-06-07 21:16:13 -0300

    desafio 2

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './CartWidget.css'
+
+const renderCartWidget = () => {
+    return(
+        <i class="bi bi-cart3"></i>
+    )
+}
+
+export default renderCartWidget
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,38 @@
+import {  Navbar, Container, Nav } from 'react-bootstrap';
+import './NavBar.css'
+import CartWidget from '../CartWidget/CartWidget'
+
+
+const renderNavBar = () =>{
+    return(
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <div className='containerLogo'>
+                        <img src='https://dummyimage.com/150x50/000/fff' />
+                    </div>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Inicio</Nav.Link>
+                    <Nav.Link href="#link">Nosotros</Nav.Link>
+                    <Nav.Link href="#home">Contactos</Nav.Link>
+                    <Nav.Link href="#link">Productos</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <a href="#login" >
+                            <CartWidget />
+                            </a>
+                            
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+
+export default renderNavBar
